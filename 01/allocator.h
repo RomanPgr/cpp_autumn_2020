e3a6801338552dec2de02651e42698deff4a680a
RomanPgr <romanpogarskii2309@yandex.ru>
2020-12-09 02:30:56 +0300

    add nxc

--- 01/allocator.h
+++ 01/allocator.h
@@ -1,14 +1,15 @@
 #ifndef ALLOCATOR_H
 #define ALLOCATOR_H
+
 #include <cstddef>
 
 class Allocator{
 public:
-    Allocator();
+    Allocator() noexcept;
     Allocator(size_t maxSize);
     void makeAllocator(size_t maxSize);
-    char* alloc(size_t size);
-    void reset();
+    char* alloc(size_t size) noexcept;
+    void reset() noexcept;
     ~Allocator();
 
 private:
@@ -16,4 +17,5 @@ private:
     size_t cur_size;
     char* beg_mem;
 };
+
 #endif
